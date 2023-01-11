3a05cbbf0c32beaf054d6f842daba2550e015551
mikeelingad <122429907+mikeelingad@users.noreply.github.com>
2023-01-10 22:32:05 -0800

    Add files via upload
    tuple beginner

---
+++ ##defing tuple.py
@@ -0,0 +1,25 @@
+##defing tuple
+dimension =(10000, 50)
+print(dimension[0])
+print(dimension[1])
+##### looping through all values tuple
+dimension2 = (1241212,1231,11,50)
+###### writing for a for loop over tuple
+print("original for tuples")
+for dimension in dimension2:
+    print(dimension2)
+#### 4-13 Buffet
+pizza = ['pepperoni','mushroom','spinach','tomatoes','onion']
+
+### print the names all the pizzas
+for pizza in pizza:
+    print(pizza)
+
+###try to modify one of the ire,s and ,ale sure that python rejects and change
+for pizza in pizza[:3]:
+    print(pizza.title())
+    print("wrong pizza you have choosen")
+#####resturant changes menu replacing two of items then use the print
+
+for pizza in pizza:
+    print(pizza)
